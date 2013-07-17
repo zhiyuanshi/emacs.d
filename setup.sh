6718a3b6b8c0152be13ddaf9e99e4da786c80676
zhiyuanshi <zhiyuanshi@server.fake>
2013-07-17 16:05:26 +0800

    bash cd to Dev on startup; add alias for gitg

--- setup.sh
+++ setup.sh
@@ -10,19 +10,19 @@ cp .hgrc ~      # dotfiles for hg
 # We don't remove the old directory, because it may contain other good things.
 cp -r .ocp ~
 
-# Remove bash configuration
-rm -rf ~/.gconf/apps/gnome-terminal
+rm -rf ~/.gconf/apps/gnome-terminal # Remove bash configuration
 
 # Ammendments to .bashrc
 
 cp /etc/skel/.bashrc ~
-echo -e "\nbind \"set completion-ignore-case on\"\n" >> ~/.bashrc
+echo -e "\ncd ~/Dropbox/Dev" >> ~/.bashrc # Desired default directory
+echo -e "bind \"set completion-ignore-case on\"\n" >> ~/.bashrc
 
 # Set OPAM's environment variables upon each startup of bash
 # https://realworldocaml.org/beta1/en/html/installation.html
 echo -e "eval \`opam config -env\`\n" >> ~/.bashrc
 
-# Redefine gvim and emacs in bash
-# Reserve the command 'vi' in case we want to use it in console mode
-echo 'function gvim ()  { (/usr/bin/gvim -f "$@" &) }' >> ~/.bashrc
-echo 'function vim  ()  { (/usr/bin/gvim -f "$@" &) }' >> ~/.bashrc
+# Re-definitions
+echo 'function gvim () { (/usr/bin/gvim -f "$@" &) }'      >> ~/.bashrc
+echo 'function vim  () { (/usr/bin/gvim -f "$@" &) }'      >> ~/.bashrc
+echo 'function gitg () { (/usr/bin/gitg 2> /dev/null &) }' >> ~/.bashrc
