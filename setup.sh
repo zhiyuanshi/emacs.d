da4ebb8a1a7d8b263e9f6d84f8821950c244786e
zhiyuanshi <zhiyuanshi@server.fake>
2013-07-22 15:09:06 +0800

    Detect OCaml directories and send '_tags' to them

--- setup.sh
+++ setup.sh
@@ -2,29 +2,36 @@
 
 cd ~/Dropbox/Dev/dotfiles
 
+# Home directory
 cp .vimrc ~     # dotfiles for Vim
 cp .ocamlinit ~ # dotfiles for OCaml
 cp .hgrc ~      # dotfiles for hg
-
-# dotfiles for ocp-indent
-# We don't remove the old directory, because it may contain other good things.
-cp -r .ocp ~
-
+cp -r .ocp ~    # We don't remove the old directory, because it may contain other good things.
 rm -rf ~/.gconf/apps/gnome-terminal # Remove bash configuration
 
-# Ammendments to .bashrc
+# Repos
+for dir in $(ls ..)
+do
+  if [ "$dir" != "dotfiles" ] && [ -d "../$dir" ]; then
+    cp .gitignore "../$dir"
+    if [ $(ack-grep -f "../$dir" -G '\.ml$' -1 | wc -l) -ne "0" ]; then
+      echo "OCaml: $dir"
+      cp _tags "../$dir"
+    fi
+  fi
+done
+
+# .bashrc
 
 cp /etc/skel/.bashrc ~
 echo -e "\ncd ~/Dropbox/Dev" >> ~/.bashrc # Desired default directory
-echo -e "bind \"set completion-ignore-case on\"\n" >> ~/.bashrc
+echo -e "bind 'set completion-ignore-case on'\n" >> ~/.bashrc
 
 # Set OPAM's environment variables upon each startup of bash
 # https://realworldocaml.org/beta1/en/html/installation.html
 echo -e "eval \`opam config -env\`\n" >> ~/.bashrc
 
-# Re-definitions
-echo 'alias vim=gvim'     >> ~/.bashrc
-echo 'alias ack=ack-grep' >> ~/.bashrc
-echo 'function gvim () { (/usr/bin/gvim -f "$@" &) }'      >> ~/.bashrc
-echo 'function vim  () { (/usr/bin/gvim -f "$@" &) }'      >> ~/.bashrc
-echo 'function gitg () { (/usr/bin/gitg 2> /dev/null &) }' >> ~/.bashrc
+echo "alias ack='ack-grep'" >> ~/.bashrc
+echo 'function gvim () { (/usr/bin/gvim "$@" 2> /dev/null &) }' >> ~/.bashrc
+echo 'function vim  () { (/usr/bin/gvim "$@" 2> /dev/null &) }' >> ~/.bashrc
+echo 'function gitg () { (/usr/bin/gitg "$@" 2> /dev/null &) }' >> ~/.bashrc
