f079e19dc853eaf4ca5a03c324e14ab37d3dd146
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2014-08-01 23:46:22 +0800

    Adjust prelude-modules.el: +prelude-helm, +prelude-ocaml, +prelude-web

--- .emacs.d/prelude-modules.el
+++ .emacs.d/prelude-modules.el
@@ -3,7 +3,7 @@
 ;; Emacs IRC client
 (require 'prelude-erc)
 (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
-;; (require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm) ;; Interface for narrowing and search
 (require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-mediawiki)
@@ -22,6 +22,7 @@
 (require 'prelude-js)
 (require 'prelude-latex)
 (require 'prelude-lisp)
+(require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
 (require 'prelude-python)
@@ -30,5 +31,5 @@
 (require 'prelude-scheme)
 (require 'prelude-shell)
 (require 'prelude-scss)
-;; (require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
