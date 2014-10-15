1fb86f3fa4c2effb6fe29e2042eee8cdf4e77904
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2014-10-15 10:49:41 +0800

    Remove install scripts from dotfiles.

--- components/cabal.sh
+++ components/cabal.sh
@@ -1,7 +1,1 @@
-# We don't like the Cabal packaged with haskell-platform.
-# if [[ $(which cabal) == "/usr/bin/cabal" ]]; then
-#   cabal update
-#   cabal install cabal-install
-# fi
-
 export PATH="$HOME/.cabal/bin:$PATH"
--- components/fzf.sh
+++ components/fzf.sh
@@ -1,6 +1,1 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-if ! defined fzf; then
-  git clone https://github.com/junegunn/fzf.git ~/.fzf
-  ~/.fzf/install
-fi
