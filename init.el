9d22c6aaa61dd90aeda848b7df9c5e751c6babbd
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2015-03-02 12:18:40 +0800

    Fix `File mode specification error: (void-function turn-on-pandoc)`

--- init.el
+++ init.el
@@ -539,13 +539,6 @@
 (eval-after-load 'haskell-mode
   '(define-key haskell-mode-map (kbd "C-c C-d") 'ac-haskell-process-popup-doc))
 
-(require 'shm)
-(require 'shm-case-split)
-(add-hook 'haskell-mode-hook 'structured-haskell-mode)
-(set-face-background 'shm-current-face "#eee8d5")
-(set-face-background 'shm-quarantine-face "lemonchiffon")
-;; (setq shm-idle-timeout 0)
-
 (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)
 
 (dolist (hook '(haskell-mode-hook inferior-haskell-mode-hook haskell-interactive-mode-hook))
@@ -687,7 +680,9 @@
 
 (setq doc-view-continuous t)
 
+(require 'pandoc-mode)
 (add-hook 'markdown-mode-hook 'turn-on-pandoc)
+(add-hook 'pandoc-mode-hook 'pandoc-load-default-settings)
 
 (setq org-confirm-babel-evaluate nil)
 (setq org-src-tab-acts-natively t)
