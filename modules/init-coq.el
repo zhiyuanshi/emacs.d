e49c761e23f2ca0dcfe99b07fb5678ec46efe225
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2015-12-07 23:47:23 +0800

    company-coq

--- modules/init-coq.el
+++ modules/init-coq.el
@@ -3,6 +3,14 @@
 (autoload 'coq-mode "coq" "Major mode for editing Coq vernacular." t)
 
 ;; As instructed by `brew install proof-general'
-(load-file "/usr/local/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
+;; (load-file "/usr/local/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
+
+;; https://github.com/cpitclaudel/company-coq
+;; Open .v files with Proof-General's coq-mode
+(require 'proof-site)
+
+;; https://github.com/cpitclaudel/company-coq
+;; Load company-coq when opening Coq files
+(add-hook 'coq-mode-hook #'company-coq-initialize)
 
 (provide 'init-coq)
